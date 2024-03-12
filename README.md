20a4e5e92d0674aa982762f039f04aae9ad61163
prgrmz07 <prgrmz07@163.com>
2024-03-12 18:27:47 +0800

    草稿中

--- README.md
+++ README.md
@@ -3,8 +3,11 @@ npm install
 
 #编译
 npm run build  
-#这个构建后的代码会在build/src目录下
- 
+#这个构建后的代码会在build/src目录下,  such as build/src/main.js
+```
+webstorm打开本项目，可以直接调试js文件 ./build/src/main.js
+
+```shell
 #运行
 npm run start
 
@@ -19,3 +22,4 @@ npm install   @types/chrome-remote-interface
 node ./main.js
 
 ```
+
--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,27 @@
 import CDP from 'chrome-remote-interface';
 import Protocol from "devtools-protocol";
 
+const urlList:string[]=[
+  "https://gitee.com/tmpOrg/projects"
+];
+
 async function interept(urlStr:string) {
   try{
     const client:CDP.Client = await CDP();
     const {Network, Page, Fetch} = client;
 
-    await Fetch.enable(  )
+    // await Fetch.enable(  )
 
     Network.on("requestWillBeSent", (params: Protocol.Network.RequestWillBeSentEvent) => {
       if(!params.request.url.startsWith("https://gitee.com")){
         return;
       }
       console.log(`【请求地址】${params.request.url}`)
+
+      if(urlList.indexOf(params.request.url)>0){
+        console.log(`【postData】【${params.request.url}】${params.request.postData}`)
+      }
+
     })
     await Network.enable();
     await Page.enable();
