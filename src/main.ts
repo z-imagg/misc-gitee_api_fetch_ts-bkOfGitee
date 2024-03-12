efa1bf0a9c5fe8681a278b32245944761ef0c28a
prgrmz07 <prgrmz07@163.com>
2024-03-12 22:17:45 +0800

    【正常】chrome-remote-interface 填充dom

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,49 @@
 
 import CDP from 'chrome-remote-interface';
 import Protocol from "devtools-protocol";
+import {Runtime} from "inspector";
+
+import * as fs from "fs";
 
 const urlList:string[]=[
   "https://gitee.com/tmpOrg/projects"
 ];
 
+const giteeAccount=JSON.parse(fs.readFileSync("gitee_account.json","utf8"))
+const giteeUserName=giteeAccount["user"];
+const giteePwd=giteeAccount["pass"];
+
+//gitee登录页面url
+const giteeLoginPageUrl="https://gitee.com/login";
+//gitee登录页面中填写用户名、填写密码的js语句，  firefox开发者工具   人工获得
+const js_fillUserPass=`
+document.getElementById("user_login").value="${giteeUserName}";
+document.getElementById("user_password").value="${giteePwd}";
+alert("已填充用户名、密码");
+`
+
 async function interept( ) {
   try{
     const client:CDP.Client = await CDP();
-    const {Network, Page,DOM, Fetch} = client;
+    const {Network, Page,DOM,Runtime, Fetch} = client;
 
     await Network.enable();
+    await Runtime.enable();
     await DOM.enable();
     await Page.enable();
-    await Page.navigate( {url:"https://gitee.com/projects/import/url"});
+    //打开gitee登录页面
+    await Page.navigate( {url:giteeLoginPageUrl});
+
+    // types/chrome-remote-interface 说 没有此方法 loadEventFired，但是 官方例子 中有此方法， https://github.com/cyrus-and/chrome-remote-interface/wiki/Async-await-example
     await Page.loadEventFired()
 
+    await DOM.getDocument();
+    //填写用户名、密码
+    await Runtime.evaluate(<Protocol.Runtime.EvaluateRequest>{
+      expression:js_fillUserPass
+    })
+
+    //请求过滤
     Network.on("requestWillBeSent", (params: Protocol.Network.RequestWillBeSentEvent) => {
       if(!params.request.url.startsWith("https://gitee.com")){
         return;
