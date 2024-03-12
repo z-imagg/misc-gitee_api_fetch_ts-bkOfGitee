d112d184fa206c0b8f450463501184ae17567d6a
prgrmz07 <prgrmz07@163.com>
2024-03-12 21:42:55 +0800

    【正常】chrome-remote-interface 填充dom（有一点嵌套）

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,21 @@
 
 import CDP from 'chrome-remote-interface';
 import Protocol from "devtools-protocol";
+import {Runtime} from "inspector";
+import * as fs from "fs";
 
 const urlList:string[]=[
   "https://gitee.com/tmpOrg/projects"
 ];
 
+const giteeAccount=JSON.parse(fs.readFileSync("gitee_account.json","utf8"))
+const giteeUserName=giteeAccount["user"];
+const giteePwd=giteeAccount["pass"];
+
 async function interept(urlStr:string) {
   try{
     const client:CDP.Client = await CDP();
-    const {Network, Page, Fetch} = client;
+    const {Network, Page, Runtime,Fetch,DOM,} = client;
 
     // await Fetch.enable(  )
 
@@ -27,11 +33,24 @@ async function interept(urlStr:string) {
     await Network.enable();
     await Page.enable();
     await Page.navigate( {url:urlStr});
-    await Page.loadEventFired()
+    // chrome-remote-interface 填充 dom , 参考 https://stackoverflow.com/questions/45500146/how-to-get-multiple-dom-elements-with-chrome-remote-interface-node-js/45513753#45513753
+    await Page.on("loadEventFired",(params: Protocol.Page.LoadEventFiredEvent)=>{
+// chrome打开页面 https://gitee.com/login ，用 开发者工具 找到 用户名输入框id、密码输入框id
+      const expr=`
+document.getElementById("user_login").value="${giteeUserName}";
+document.getElementById("user_password").value="${giteePwd}";
+alert("已填充用户名、密码");
+`
+      Runtime.evaluate(<Protocol.Runtime.EvaluateRequest>{
+        expression:expr
+      })
+
+    })
 
   }catch(err){
     console.error(err);
   }
 }
 
-interept("https://gitee.com/projects/import/url")
+// interept("https://gitee.com/projects/import/url")
+interept("https://gitee.com/login")
