05273b72b3c4249f082a178d91ee3de9bb14d344
prgrmz07 <prgrmz07@163.com>
2024-03-12 17:33:43 +0800

    草稿中

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,32 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+
+import CDP from 'chrome-remote-interface';
+import Protocol from "devtools-protocol";
+
+async function interept(urlStr:string) {
+  try{
+    let client:CDP.Client = await CDP();
+    const {Network, Page, Fetch} = client;
+    Network.on("requestWillBeSent", (params: Protocol.Network.RequestWillBeSentEvent) => {
+      console.log(`【请求地址】${params.request.url}`)
+    })
+
+    await Fetch.enable(<Protocol.Fetch.EnableRequest>{
+      patterns:[
+        <Protocol.Fetch.RequestPattern>{
+          urlPattern:"*",
+          resourceType:"XHR"
+        }
+      ]
+    })
+
+    await Network.enable();
+    await Page.enable();
+    await Page.navigate(<Protocol.Page.NavigateRequest>{url:urlStr});
+    // await Page.loadEventFired()
+
+  }catch(err){
+    console.error(err);
+  }
 }
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
-  );
-}
-
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
-
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export async function greeter(name: any) { // eslint-disable-line @typescript-eslint/no-explicit-any
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
-}
+interept("https://gitee.com/projects/import/url")
