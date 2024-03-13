3c082b9b99dc55f215413d535bcd4523cbf35863
prgrmz07 <prgrmz07@163.com>
2024-03-13 22:36:05 +0800

    删除无用

--- src/Util.ts
+++
@@ -1,7 +0,0 @@
-// nodejs版本>=9.3时， 阻塞式sleep实现如下，参考  https://www.npmjs.com/package/sleep
-function msleep(ms) {
-  Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, ms);
-}
-function sleep(seconds) {
-  msleep(seconds*1000);
-}
