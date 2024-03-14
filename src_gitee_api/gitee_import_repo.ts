377661e3448d3782ad5c8f044aac17036f37ff15
prgrmz07 <prgrmz07@163.com>
2024-03-14 13:44:28 +0800

    【调用草稿】4

--- src_gitee_api/gitee_import_repo.ts
+++ src_gitee_api/gitee_import_repo.ts
@@ -3,6 +3,8 @@ import {reqTemplDir} from "../src/my_cfg.js";
 import {readdirSync, readFileSync} from "fs";
 import {MarkupFieldUtilC} from "./MarkupFieldUtil.js";
 
+import * as DP from "devtools-protocol";
+
 const exitCode_1:number=21
 const errMsg_1:string=`【错误】【退出代码${exitCode_1}】目录【${reqTemplDir}】下没有已markup的请求例子，请你先执行脚本gen_gitee_import_repo_req_template.sh以生成请求例子`
 const exitCode_2:number=22
@@ -16,6 +18,10 @@ if(reqTmplFNLs.length<=0){
 
 const ARG_START_IDX:number=2
 const ARG_CNT:number=4
+process.argv.push("http://xxx.com/yyy.git")
+process.argv.push("org01")
+process.argv.push("repo01")
+process.argv.push("仓库描述")
 
 if (process.argv.length<ARG_START_IDX+ARG_CNT){
   console.log(errMsg_1)
@@ -42,17 +48,24 @@ const reqTmplFP:string=`${reqTemplDir}/${reqTmplFN}`
 const reqTmplText:string= readFileSync(reqTmplFP).toString()
 const rqTpl:ReqTemplateI=JSON.parse(reqTmplText)
 
-MarkupFieldUtilC.assign_(rqTpl.markupFieldLs,newFieldLs)
 
 switch (rqTpl.templatePlace){
   case TemplPlaceE.Url:{
     // rqTpl.req.url
+    MarkupFieldUtilC.assign_L2R(rqTpl.markupFieldLs,newFieldLs,rqTpl.req.url)
     break;
   }
   case TemplPlaceE.ReqHeader:{
+    const tupleLs:[string,string][]=Object.entries(rqTpl.req.headers).map(([key,val],idx)=>{
+      const newKey:string=MarkupFieldUtilC.assign_L2R(rqTpl.markupFieldLs,newFieldLs,key)
+      const newVal:string=MarkupFieldUtilC.assign_L2R(rqTpl.markupFieldLs,newFieldLs,val)
+      return [newKey,newVal]
+    })
+    rqTpl.req.headers=new DP.Protocol.Network.Headers(newFieldLs)
     break;
   }
   case TemplPlaceE.Body:{
+    rqTpl.req.postData=MarkupFieldUtilC.assign_L2R(rqTpl.markupFieldLs,newFieldLs,rqTpl.req.postData)
     break;
   }
 }
