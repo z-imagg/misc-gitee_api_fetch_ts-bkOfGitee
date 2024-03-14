850cdf7d2c6dfb9f715165fe47d703acc07cdeef
prgrmz07 <prgrmz07@163.com>
2024-03-14 12:51:09 +0800

    【调用草稿】2

--- src_gitee_api/gitee_import_repo.ts
+++ src_gitee_api/gitee_import_repo.ts
@@ -2,17 +2,20 @@
 import {ReqTemplateI} from "../src/req_tmpl_t.js";
 import {reqTemplDir} from "../src/my_cfg.js";
 import {readdirSync} from "fs";
+import {readFileSync} from "fs";
 
 const exitCode_1:number=20
 const errMsg_1:string=`【错误】【退出代码${exitCode_1}】目录【${reqTemplDir}】下没有已markup的请求例子，请你先执行脚本gen_gitee_import_repo_req_template.sh以生成请求例子`
 
-const reqTmplFpLs:string[]=readdirSync(`${reqTemplDir}`)
-if(reqTmplFpLs.length<=0){
+const reqTmplFNLs:string[]=readdirSync(`${reqTemplDir}`)
+if(reqTmplFNLs.length<=0){
   console.log(errMsg_1)
   process.exit(exitCode_1)
 }
 
-const reqTmplFp:string=reqTmplFpLs[0]
-const reqTmpl:ReqTemplateI=JSON.parse(reqTmplFp)
+const reqTmplFN:string=reqTmplFNLs[0]
+const reqTmplFP:string=`${reqTemplDir}/${reqTmplFN}`
+const reqTmplText:string= readFileSync(reqTmplFP).toString()
+const reqTmpl:ReqTemplateI=JSON.parse(reqTmplText)
 const _end:boolean=true
 
