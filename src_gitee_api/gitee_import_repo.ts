b352a279ebdbff14441a32bc5535e4f64c12f61b
prgrmz07 <prgrmz07@163.com>
2024-03-16 13:55:09 +0800

    草稿

--- src_gitee_api/gitee_import_repo.ts
+++ src_gitee_api/gitee_import_repo.ts
@@ -15,23 +15,34 @@ import assert from "assert";
 import {Command} from "commander"
 import {siteBaseUrl} from "../src/site_gitee_cfg.js";
 
-const program = new Command("【导入github仓库到gitee（基于gitee页面导入url的请求标记例子）】gitee_import_repo.ts")
 const exitCode_1:number=21
 const errMsg_1:string=`【错误】【退出代码${exitCode_1}】目录【${reqTemplDir}】下没有已markup的请求例子，请你先执行脚本script/gen_gitee_import_repo_req_template.sh以生成请求例子`
 const exitCode_2:number=22
 const errMsg_2:string=`【错误】【退出代码${exitCode_2}】命令用法为 me.js from_repoUrl goal_OrgName goal_repoName  goal_repoDesc`
 
+function MyMain(){
 
+  const program = new Command("【导入github仓库到gitee（基于gitee页面导入url的请求标记例子）】gitee_import_repo.ts")
 
-program
-  .requiredOption("-f --from_repo <from_repo>","【来源，仓库地址,常为github仓库地址】" )  //"https://github.com/request/request.git",  0 from_repo markup_project_import_url
-  .requiredOption("-o, --goal_org <goal_org>","【目标，gitee的组织】")  //"mirrr",  1 goal_org markup_project_namespace_path
-  .requiredOption("-r, --goal_repoPath <goal_repoPath>","【目标，gitee仓库路径】")  //"repo01Path", 2 goal_repoPath markup_project_path
-  .requiredOption("-n, --goal_repoName <goal_repoName>","【目标，gitee仓库名字】")  //"repo01Name", 3 goal_repoName markup_project_name
-  .requiredOption("-d, --goal_repoDesc <goal_repoDesc>","【目标，gitee仓库描述】")  //"仓库描述", 4 goal_repoDesc markup_project_description
+  program
+    .requiredOption("-f --from_repo <from_repo>","【来源，仓库地址,常为github仓库地址】" )  //"https://github.com/request/request.git",  0 from_repo markup_project_import_url
+    .requiredOption("-o, --goal_org <goal_org>","【目标，gitee的组织】")  //"mirrr",  1 goal_org markup_project_namespace_path
+    .requiredOption("-r, --goal_repoPath <goal_repoPath>","【目标，gitee仓库路径】")  //"repo01Path", 2 goal_repoPath markup_project_path
+    .requiredOption("-n, --goal_repoName <goal_repoName>","【目标，gitee仓库名字】")  //"repo01Name", 3 goal_repoName markup_project_name
+    .requiredOption("-d, --goal_repoDesc <goal_repoDesc>","【目标，gitee仓库描述】")  //"仓库描述", 4 goal_repoDesc markup_project_description
 
-program.parse()
-const options = program.opts()
+  program.parse()
+  const options = program.opts()
+
+  const markup_project_import_url:string=options.from_repo // 0
+  const markup_project_namespace_path:string=options.goal_org // 1
+  const markup_project_path:string=options.goal_repoPath // 2
+  const markup_project_name:string=options.goal_repoName // 3
+  const markup_project_description:string=options.goal_repoDesc // 4
+
+  console.log(`【命令行参数打印】${options},${markup_project_import_url}, ${markup_project_namespace_path}, ${markup_project_path}, ${markup_project_name}, ${markup_project_description}`)
+
+}
 
 
 interface SimpleRespI{
@@ -46,13 +57,7 @@ function GiteeImportRepoF(markup_project_import_url:string,markup_project_namesp
     process.exit(exitCode_1)
   }
 
-  // const markup_project_import_url:string=options.from_repo // 0
-  // const markup_project_namespace_path:string=options.goal_org // 1
-  // const markup_project_path:string=options.goal_repoPath // 2
-  // const markup_project_name:string=options.goal_repoName // 3
-  // const markup_project_description:string=options.goal_repoDesc // 4
 
-  console.log(`【命令行参数打印】${options},${markup_project_import_url}, ${markup_project_namespace_path}, ${markup_project_path}, ${markup_project_name}, ${markup_project_description}`)
 
   const newFieldLs: MarkupFieldI[]=[
     <MarkupFieldI>{fldNm:"markup_project_import_url",fldVal:encodeURIComponent(markup_project_import_url) },
