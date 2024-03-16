1c2965553a8020bdfc466e977e9dff1b286fb6c1
prgrmz07 <prgrmz07@163.com>
2024-03-17 00:19:11 +0800

    草稿

--- script/bash-complete--import_githubRepo_to_gitee.sh
+++ script/bash-complete--import_githubRepo_to_gitee.sh
@@ -10,7 +10,7 @@ _import_githubRepo_to_gitee() {
     #cur="$2"
     pre=${COMP_WORDS[COMP_CWORD-1]}
     cur=${COMP_WORDS[COMP_CWORD]}
-    opts="--from_repo --goal_org --goal_repoPath --goal_repoName --goal_repoDesc -h --help"
+    opts="--from_repo --goal_org --goal_repoPath --goal_repoName --goal_repoDesc  --write_return -h --help"
     case "$cur" in
     -* )
         COMPREPLY=( $( compgen -W "$opts" -- $cur ) )
