5abbdb605b9c937240e49f930a3153d8dfba5709
prgrmz07 <prgrmz07@163.com>
2024-04-24 14:45:29 +0800

    use_example.sh

--- script/use_example.sh
+++ script/use_example.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
 
-#【用法】 source use_example.sh
+#【用法】 bash use_example.sh
+echo "\$0=$0"
 
-source <(curl http://giteaz:3000/bal/bash-simplify/raw/branch/release/cdCurScriptDir.sh)
+source <(curl --silent http://giteaz:3000/bal/bash-simplify/raw/branch/release/cdCurScriptDir.sh)
 cdCurScriptDir
 
+local err01_code=23
+local err01_txt="error : /app/bin/ not in \$PATH, exit $err01_code"
+
+#断言'/app/bin/' 必须在$PATH中
+echo $PATH | grep "/app/bin" >/dev/null || { echo "$err01_txt" && exit $err01_code ;}
 
 #导入github仓库到gitee
 # export PATH=/app/github-gitee-GITEA/gitee_api_fetch_ts/script:$PATH
+local SrcF=/app/github-gitee-GITEA/gitee_api_fetch_ts/script/import_githubRepo_to_gitee.sh  && \
+local F=/app/bin/import_githubRepo_to_gitee.sh && \
+{ unlink $F || ln -s $SrcF $F ;} 
 
 #生成bash命令import_githubRepo_to_gitee.sh的提示
 source bash-complete--import_githubRepo_to_gitee.sh
+
+readlink -f `which import_githubRepo_to_gitee.sh`
 
 import_githubRepo_to_gitee.sh --help
 
